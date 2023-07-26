0c626f421cde122e788c3f7d6fd7c3c7243cc59a
yogita <yogitakulkarni2112000@gmail.com>
2023-07-26 06:23:35 +0530

    gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,3 @@
-// // App.tsx
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import SearchForm from './components/SearchForm';
-// import GifList from './components/GifList';
-// import { Gif, ApiResponse, GifData } from './types';
-// import './styles.css';
-
-// const App: React.FC = () => {
-//   const [gifs, setGifs] = useState<Gif[]>([]);
-
-//   const handleSearch = (query: string) => {
-//     const apiKey = 'sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh';
-//     const url = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}&limit=10`;
-
-//     axios
-//       .get<ApiResponse>(url)
-//       .then((response) => {
-//         const gifsData = response.data.data;
-//         console.log(gifsData)
-//         const gifsList: Gif[] = gifsData.map((gifData: GifData) => ({
-//           id: gifData.title,
-//           url: gifData.images.fixed_height.url,
-//           title: gifData.title,
-//         }));
-//         setGifs(gifsList);
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching GIFs:', error);
-//       });
-//   };
-
-//   return (
-//     <div className="container">
-//       <h1 className="title">GIF Search </h1>
-//       <div className="search-container">
-//         <SearchForm onSearch={handleSearch} />
-//       </div>
-//       <GifList gifs={gifs} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-// App.tsx
-// App.tsx
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import SearchForm from './components/SearchForm';
@@ -129,3 +82,6 @@ const App: React.FC = () => {
 };
 
 export default App
+
+
+
--- src/components/GifList.tsx
+++ src/components/GifList.tsx
@@ -1,34 +1,3 @@
-// // GifList.tsx
-// import React from 'react';
-// import '../styles.css';
-
-// interface Gif {
-//   id: string;
-//   url: string;
-//   title: string;
-// }
-
-// interface GifListProps {
-//   gifs: Gif[];
-// }
-
-// const GifList: React.FC<GifListProps> = ({ gifs }) => {
-//   return (
-//     <div className="gif-list">
-//       {gifs.map((gif) => (
-//         <div key={gif.id} className="gif-item">
-//           <img className="gif-img" src={gif.url} alt={gif.title} />
-//           {/* <div className="gif-title">{gif.title}</div> */}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default GifList;
-
-
-// GifList.tsx
 import React from 'react';
 import '../styles.css';
 interface Gif {
